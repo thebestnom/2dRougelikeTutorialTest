17a9aad9b725cda623e4d9a171822cc5606ea0fd
thebestnom <shoval.arad@gmail.com>
2020-01-22 00:40:14 +0200

    almost ready

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float turnDelay = .1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoint = 100;
+    [HideInInspector] public bool playerTurn = true;
 
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 
     private void Awake()
     {
@@ -22,12 +27,53 @@ public class GameManager : MonoBehaviour
         }
         
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
 
     private void InitGame()
     {
+        enemies.Clear();
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+    
+    void Update()
+    {
+        if (playerTurn || enemiesMoving)
+        {
+            return;
+        }
+
+        StartCoroutine(MoveEnemies());
+    }
+    
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        foreach (var enemy in enemies)
+        {
+            enemy.MoveEnemy();
+            yield return new WaitForSeconds(enemy.moveTime);
+        }
+
+        playerTurn = true;
+        enemiesMoving = false;
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MovingObject
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+    
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+    private static readonly int PlayerChop = Animator.StringToHash("playerChop");
+    private static readonly int PlayerHit = Animator.StringToHash("playerHit");
+
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        food = GameManager.instance.playerFoodPoint;
+        
+        base.Start();
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.playerFoodPoint = food;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameManager.instance.playerTurn) return;
+
+        var horizontal = (int) Input.GetAxisRaw(HORIZONTAL);
+        var vertical =  (int) Input.GetAxisRaw(VERTICAL);
+
+        if (horizontal != 0) vertical = 0;
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            AttemptMove<Wall>(horizontal, vertical);
+        }
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+        base.AttemptMove<T>(xDir, yDir);
+        RaycastHit2D hit;
+        CheckIfGameOver();
+        GameManager.instance.playerTurn = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.tag)
+        {
+            case "Exit":
+                Invoke("Restart", restartLevelDelay);
+                break;
+            case "Food":
+                food += pointsPerFood;
+                other.gameObject.SetActive(false);
+                break;
+            case "Soda":
+                food += pointsPerSoda;
+                other.gameObject.SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        var hitWall = component as Wall;
+        // ReSharper disable once PossibleNullReferenceException
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger(PlayerChop);
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoseFood(int loss)
+    {
+        animator.SetTrigger(PlayerHit);
+        food -= loss;
+        CheckIfGameOver();
+    }
+
+    private void CheckIfGameOver()
+    {
+        if(food <= 0) GameManager.instance.GameOver();
+    }
+}
